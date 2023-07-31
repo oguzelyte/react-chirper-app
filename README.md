97675ffea8dbf545c3b87dd24f38a53bd7a20ab9
Olivija Guzelyte <olivija.guzelyte@kilo.health>
2023-07-31 01:57:32 +0100

    add project files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Chirper Project
+
+This repo is a code-along with the first project in the React Nanodegree program.
+This is a basic 'Twitter'-like program created to learn the fundamentals of Redux.
+Most of the commits in this repository correspond to videos in the program.
+
+## Project Setup
+
+- Clone the Project.
+- Install the dependencies: `npm install`
---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import { useEffect, Fragment } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import LoadingBar from "react-redux-loading-bar";
+import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+import { Routes, Route } from "react-router-dom";
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <Fragment>
+      <LoadingBar />
+      <div className="container">
+        <Nav />
+        {props.loading === true ? null : (
+          <Routes>
+            <Route path="/" exact element={<Dashboard />} />
+            <Route path="/tweet/:id" element={<TweetPage />} />
+            <Route path="/new" element={<NewTweet />} />
+          </Routes>
+        )}
+      </div>
+    </Fragment>
+  );
+};
+
+const mapStateToProps = ({ authedUser }) => ({
+  loading: authedUser === null,
+});
+
+export default connect(mapStateToProps)(App);
